0d0d1e7f795af23163bc493a00feb467768e00ad
Giova Salazar <nesalaz56@gmail.com>
2024-09-10 15:59:53 -0500

    se muestran imagenes en card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     GifsModule,
     SharedModule
   ],
--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../service/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
-
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
 }
