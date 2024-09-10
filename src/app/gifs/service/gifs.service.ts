7c3c4b54325cb66e3ea76ff7b9b7224d2a69de17
Giova Salazar <nesalaz56@gmail.com>
2024-09-10 17:04:52 -0500

    termina seccion 7

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
   private apiKey: string = 'JNbATTC2XQ35li4QXnVJbLkKoCRtyTNG';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStore();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -25,6 +27,18 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStore();
+  }
+
+  private saveLocalStore(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStore(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
